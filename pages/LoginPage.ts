3f7892126758b0bc93edf6d092b05f7681922037
Obied <Akel.Obied@philips.com>
2025-03-23 16:07:10 +0200

    adding logout message

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -6,6 +6,9 @@ export class LoginPage extends BasePage {
   private passwordInput = this.page.locator('#password');
   private loginButton = this.page.locator('#login button');
   private flashMessage = this.page.locator('#flash b');
+  private flashMessageLogout = this.page.locator('id=flash');
+  private logoutButton =  this.page.getByRole('link', { name: 'logout' });
+  
 
   constructor(page: Page) { 
     super(page);
@@ -17,7 +20,15 @@ export class LoginPage extends BasePage {
     await this.loginButton.click();
   }
 
+  async logout() {
+    await this.logoutButton.click();
+  }
+
    getFlashMessage() {
     return this.page.locator('#flash b');
+  }
+
+  getFlashLogoutMessage() {
+    return this.flashMessageLogout;
   }
 }
--- tests/login-tests.spec.ts
+++ tests/login-tests.spec.ts
@@ -15,3 +15,12 @@ test('Invalid Login', async ({loginPage})=>{
   await expect(loginPage.getFlashMessage()).toContainText("Your password is invalid!");
 
 });
+
+test('Logout', async ({loginPage})=>{
+  
+  await loginPage.goto('https://practice.expandtesting.com/login');
+  await loginPage.login('practice', 'SuperSecretPassword!');
+  await loginPage.logout();
+  await expect(loginPage.getFlashLogoutMessage()).toContainText("You logged out of the secure area!");
+
+});
